b25e500ff8ae1bca611b5bdf17d0432c4165c781
f_ms <imf_ms@yeah.net>
2017-07-16 05:36:38 +0800

    add none library

---
+++ none-library/src/main/java/cn/f_ms/none_library/None.java
@@ -0,0 +1,14 @@
+package cn.f_ms.none_library;
+
+/**
+ * None
+ *
+ * @author imf_m
+ * @time 2017/7/15
+ */
+
+public final class None {
+    private static final None NONE = new None();
+    private None() {}
+    public static final None NONE() { return NONE; };
+}
